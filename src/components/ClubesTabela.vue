d77647eadf241092ab7f2dd730f02e53ea75836f
carolinelds <carolinelds@gmail.com>
2022-03-18 15:49:41 -0300

    Componetiza a tabela de classificação dos clubes

---
+++ src/components/ClubesTabela.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th colspan="2" class="text-left">Clube</th>
+            <th class="text-right">Pontos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(clube, index) in clubes" :key="clube.id">
+            <td>{{ index + 1 }}</td>
+            <td>
+              <v-avatar size="24">
+                <img :src="clube.escudo" :alt="clube.nome" />
+              </v-avatar>
+              <span class="pl-2">{{ clube.nome }}</span>
+            </td>
+            <td class="text-right">{{ clube.pontos }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ClubesTabela',
+  props: {
+    clubes: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/ClubesLista.vue
+++ src/views/ClubesLista.vue
@@ -2,37 +2,19 @@
   <v-container>
     <h2 class="text-h5 text-center mt-3 mb-5">Classificação</h2>
 
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th colspan="2" class="text-left">Clube</th>
-            <th class="text-right">Pontos</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(clube, index) in clubesOrdenados" :key="clube.id">
-            <td>{{ index + 1 }}</td>
-            <td>
-              <v-avatar size="24">
-                <img
-                  :src="clube.escudo"
-                  :alt="clube.nome"
-                />
-              </v-avatar>
-              <span class="pl-2">{{ clube.nome }}</span>
-            </td>
-            <td class="text-right">{{ clube.pontos }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <ClubesTabela :clubes="clubesOrdenados"/>
+    
   </v-container>
 </template>
 
 <script>
+import ClubesTabela from '../components/ClubesTabela.vue'
+
 export default {
   name: "ClubesLista",
+  components: {
+      ClubesTabela
+  },
   data() {
     return {
       clubesLista: [],
